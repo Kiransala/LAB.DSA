d5b80ed8b0d30ae0a2debd5f3d5452981ef5c33d
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-08-25 11:36:03 +0530

    Add files via upload

---
+++ queue1.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+# define SIZE 100
+
+void enqueue();
+void dequeue();
+void display();
+
+int array[SIZE];
+int Rear = - 1;
+int Front = - 1;
+
+main()
+{
+    int choice;
+    while (1)
+    {
+        printf("1.Enqueue Operation\n");
+        printf("2.Dequeue Operation\n");
+        printf("3.Display the Queue\n");
+        printf("4.Exit\n");
+        printf("Enter your choice of operations : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+            case 1:
+            enqueue();
+            printf("__________________________________\n");
+            break;
+            case 2:
+            dequeue();
+            printf("__________________________________\n");
+            break;
+            case 3:
+            display();
+            printf("__________________________________\n");
+            break;
+            case 4:
+            printf("Have a good day.\n");
+            exit(0);
+            default:
+            printf("Incorrect choice \n");
+        } 
+    } 
+} 
+ 
+void enqueue()
+{
+    int insert_item;
+    if (Rear == SIZE - 1)
+       printf("Overflow \n");
+    else
+    {
+        if (Front == - 1)
+        Front = 0;
+        printf("Element to be inserted in the Queue : ");
+        scanf("%d", &insert_item);
+        Rear = Rear + 1;
+        array[Rear] = insert_item;
+    }
+} 
+ 
+void dequeue()
+{
+    if (Front == - 1 || Front > Rear)
+    {
+        printf("Underflow \n");
+        return ;
+    }
+    else
+    {
+        printf("Element deleted from the Queue: %d\n", array[Front]);
+        Front = Front + 1;
+    }
+} 
+ 
+void display()
+{
+    
+    if (Front == - 1)
+        printf("Empty Queue \n");
+    else
+    {
+        printf("Queue: \n");
+        for (int i = Front; i <= Rear; i++)
+            printf("%d ", array[i]);
+        printf("\n");
+    }
+}
