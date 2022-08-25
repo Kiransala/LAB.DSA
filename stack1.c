903c919cb2279c3256522520493cf5853769f2e5
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-08-25 21:23:44 +0530

    Update stack1.c

--- stack1.c
+++ stack1.c
@@ -1,105 +1,77 @@
-#include <stdio.h>
+#include<stdio.h>
+#define Size 100
+ 
+int Top=-1, array[Size];
 
-int MAXSIZE = 8;       
-int stack[8];     
-int top = -1;            
-
-int isempty() 
+void Push();
+void Pop();
+void Trav();
+ 
+int main()
 {
-
-   if(top == -1)
-    return 1;
-   else
-    return 0;
-}
-   
-int isfull() 
-{
-
-   if(top == MAXSIZE)
-    return 1;
-   else
-    return 0;
-}
-
-int peek() 
-{
-   return stack[top];
-}
-
-int pop() 
-{
-   int data;
+	int choice;
 	
-   if(!isempty()) 
-   {
-      data = stack[top];
-      top = top - 1;   
-      return data;
-   } else 
-   {
-      printf("Could not retrieve data, Stack is empty.\n");
-   }
+	while(1)	
+	{
+		printf("\nOperations performed by Stack");
+		printf("\n1.Push the element\n2.Pop the element\n3.Traverse\n4.Exit");
+		printf("\n\nEnter the choice: ");
+		scanf("%d",&choice);
+		
+		switch(choice)
+		{
+			case 1: Push();
+				break;
+			case 2: Pop();
+				break;
+			case 3: Trav();
+				break;
+			case 4: return 0;
+			
+			default: printf("\nInvalid choice!!");
+		}
+	}
 }
-
-int push(int data) 
+ 
+void Push()
 {
-
-   if(!isfull()) 
-   {
-      top = top + 1;   
-      stack[top] = data;
-   } else 
-   {
-      printf("Could not insert data, Stack is full.\n");
-   }
+	int x;
+	if(Top==Size-1)
+	{
+		printf("\nOverflow!!");
+	}
+	else
+	{
+		printf("\nEnter element to be inserted to the stack: ");
+		scanf("%d",&x);
+		Top=Top+1;
+		array[Top]=x;
+	}
 }
-
-int main() 
+ 
+void Pop()
 {
-   int i;
-   int n;
-   int no;
-   int maxop;
-   printf("How many operation would you like to perform?\n");
-   scanf("%d",&maxop);
-   for(i = 1; i <= maxop ; i++)
-   {
-   printf("Choose one \n1.push 2.pop :");
-   scanf("%d",&no);
-   // using switch case
-   switch (no)
-       {
-          case 1:
-          printf("Enter a number,Push:");
-          scanf("%d",&n);
-          printf(" ");
-          push(n);
-          break;
-          
-          case 2:
-          pop(n);
-          break;
-          
-          default:
-          printf("Invalid choice, Please enter valid number.\n");
-          break;
-       }
-  
-   }
-   
-   printf("Element at top of the stack: %d\n" ,peek());
-   printf("Elements: \n");
-
-   // print stack data 
-   while(!isempty()) 
-   {
-      int data = pop();
-      printf("%d\n",data);
-   }
-
-   printf("Stack full: %s\n" , isfull()?"true":"false");
-   printf("Stack empty: %s\n" , isempty()?"true":"false");
-   
-   return 0;
+	if(Top==-1)
+	{
+		printf("\nUnderflow!!");
+	}
+	else
+	{
+		printf("\nPopped element:  %d",array[Top]);
+		Top=Top-1;
+	}
+}
+ 
+void Trav()
+{
+	if(Top==-1)
+	{
+		printf("\nUnderflow!!");
+	}
+	else
+	{
+		printf("\nElements present in the stack: \n");
+		for(int i=Top;i>=0;--i)
+			printf("%d\n",array[i]);
+	}
 }
