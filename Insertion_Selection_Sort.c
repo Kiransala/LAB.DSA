7d82099adcd1d53e1a3bfd710f1bdf1ef9fad706
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-11-03 11:39:21 +0530

    Update Insertion_Selection_Sort.c

--- Insertion_Selection_Sort.c
+++ Insertion_Selection_Sort.c
@@ -1,78 +1,81 @@
- #include <stdio.h>  
-      
-    void insert(int a[], int n) /* function to sort an aay with insertion sort */  
-    {  
-        int i, j, temp;  
-        for (i = 1; i < n; i++) {  
-            temp = a[i];  
-            j = i - 1;  
-      
-            while(j>=0 && temp <= a[j])  /* Move the elements greater than temp to one position ahead from their current position*/  
-            {    
-                a[j+1] = a[j];     
-                j = j-1;    
-            }    
-            a[j+1] = temp;    
-        }  
-    }  
-      
-    void printArr(int a[], int n) /* function to print the array */  
-    {  
-        int i;  
-        for (i = 0; i < n; i++)  
-            printf("%d ", a[i]);  
-    }  
-      
-    int main()  
-    {  
-        int a[] = { 12, 31, 25, 8, 32, 17 };  
-        int n = sizeof(a) / sizeof(a[0]);  
-        printf("Before sorting array elements are - \n");  
-        printArr(a, n);  
-        insert(a, n);  
-        printf("\nAfter sorting array elements are - \n");    
-        printArr(a, n);  
-      
-        return 0;  
-    }    
+// Insertion sort in C
+
+#include <stdio.h>
+
+// Function to print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
+
+void insertionSort(int array[], int size) {
+  for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
+
+    // Compare key with each element on the left of it until an element smaller than
+    // it is found.
+    // For descending order, change key<array[j] to key>array[j].
+    while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
+}
+
+// Driver code
+int main() {
+  int data[] = {9, 5, 1, 4, 3};
+  int size = sizeof(data) / sizeof(data[0]);
+  insertionSort(data, size);
+  printf("Sorted array in ascending order:\n");
+  printArray(data, size);
+}
 
 
+// Selection sort in C
 
-#include <stdio.h>  
-  
-void selection(int arr[], int n)  
-{  
-    int i, j, small;  
-      
-    for (i = 0; i < n-1; i++)    // One by one move boundary of unsorted subarray  
-    {  
-        small = i; //minimum element in unsorted array  
-          
-        for (j = i+1; j < n; j++)  
-        if (arr[j] < arr[small])  
-            small = j;  
-// Swap the minimum element with the first element  
-    int temp = arr[small];  
-    arr[small] = arr[i];  
-    arr[i] = temp;  
-    }  
-}  
-  
-void printArr(int a[], int n) /* function to print the array */  
-{  
-    int i;  
-    for (i = 0; i < n; i++)  
-        printf("%d ", a[i]);  
-}  
-  
-int main()  
-{  
-    int a[] = { 12, 31, 25, 8, 32, 17 };  
-    int n = sizeof(a) / sizeof(a[0]);  
-    printf("Before sorting array elements are - \n");  
-    printArr(a, n);  
-    selection(a, n);  
-    printf("\nAfter sorting array elements are - \n");    
-    printArr(a, n);  
-    return 0;  
+#include <stdio.h>
+
+// function to swap the the position of two elements
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void selectionSort(int array[], int size) {
+  for (int step = 0; step < size - 1; step++) {
+    int min_idx = step;
+    for (int i = step + 1; i < size; i++) {
+
+      // To sort in descending order, change > to < in this line.
+      // Select the minimum element in each loop.
+      if (array[i] < array[min_idx])
+        min_idx = i;
+    }
+
+    // put min at the correct position
+    swap(&array[min_idx], &array[step]);
+  }
+}
+
+// function to print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+
+// driver code
+int main() {
+  int data[] = {20, 12, 10, 15, 2};
+  int size = sizeof(data) / sizeof(data[0]);
+  selectionSort(data, size);
+  printf("Sorted array in Acsending Order:\n");
+  printArray(data, size);
 }
