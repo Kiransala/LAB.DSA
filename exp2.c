df8bc1ecc1c7a86aaaf616af0bb524ff317ce97b
Kiran Sala <95543329+Kiransala@users.noreply.github.com>
2022-11-16 05:17:56 +0530

    Update exp2.c

--- exp2.c
+++ exp2.c
@@ -1,84 +1,82 @@
-#include <stdio.h>
-# define SIZE 100
+#include<stdio.h>
+#define SIZE 100
+
+int Front = -1, Rear = -1, array[SIZE];
 
 void enqueue();
 void dequeue();
 void display();
 
-int array[SIZE];
-int Rear = - 1;
-int Front = - 1;
-
 main()
 {
     int choice;
-    while (1)
+    
+    while(1)
     {
-        printf("1.Enqueue Operation\n");
-        printf("2.Dequeue Operation\n");
-        printf("3.Display the Queue\n");
-        printf("4.Exit\n");
-        printf("Enter your choice of operations : ");
-        scanf("%d", &choice);
-        switch (choice)
+        printf("\n\n1.Enqueue \n2.Dequeue \n3.Display \n4.Exit \nEnter choice: ");
+        scanf("%d",&choice);
+        switch(choice)
         {
-            case 1:
-            enqueue();
+            case 1: enqueue();
             break;
-            case 2:
-            dequeue();
+            case 2: dequeue();
             break;
-            case 3:
-            display();
+            case 3: display();
             break;
-            case 4:
-            exit(0);
-            default:
-            printf("Incorrect choice \n");
-        } 
-    } 
-} 
- 
+            case 4: return 0;
+            
+            default: printf("Invalid choice.");
+        }
+    }
+    
+}
+
 void enqueue()
 {
-    int insert_item;
-    if (Rear == SIZE - 1)
-       printf("Overflow \n");
+    int x;
+    
+    if (Rear == SIZE-1)
+    {
+        printf("Queue if full.");
+    }
     else
     {
-        if (Front == - 1)
+        if (Front == -1)
         Front = 0;
-        printf("Element to be inserted in the Queue : ");
-        scanf("%d", &insert_item);
+        printf("Enter the element: ");
+        scanf("%d",&x);
         Rear = Rear + 1;
-        array[Rear] = insert_item;
+        array[Rear]=x;
     }
-} 
- 
+}
+
 void dequeue()
 {
-    if (Front == - 1 || Front > Rear)
+    int x;
+    
+    if (Front == -1 || Front > Rear)
     {
-        printf("Underflow \n");
-        return ;
+        printf("Stack is empty.");
     }
     else
     {
-        printf("Element deleted from the Queue: %d\n", array[Front]);
+        printf("\nDeleted element: %d",array[Front]);
         Front = Front + 1;
     }
-} 
- 
+}
+
 void display()
 {
-    
-    if (Front == - 1)
-        printf("Empty Queue \n");
+    if (Front == -1)
+    {
+        printf("Stack is empty.");
+    }
     else
     {
-        printf("Queue: \n");
-        for (int i = Front; i <= Rear; i++)
-            printf("%d ", array[i]);
-        printf("\n");
+        printf("\nElements in Queue: \n");
+        for(int i = Front ; i <= Rear ; i++)
+        {
+            printf("%d\n",array[i]);
+        }
     }
 }
